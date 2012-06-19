be1fa4a21a9d38e7772af9df598aafd4d01beb2b
Matt Briggs <matt@mattbriggs.net>
2012-06-19 17:00:41 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WhoHasTheBall::Application.config.secret_token = 'fffc575b0631d99fe4ac25d708e9248ee8d041b4d96a9af78a5e04330655c567c2df49763fc81a8e3aa5697fc4d9771f1a060372f45fcafc8c79975e1e2e525b'
