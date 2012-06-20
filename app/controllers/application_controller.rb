f7e3da26eaf02cb999a4aa5511c4a43d4dfde9ee
Matt Briggs <matt@mattbriggs.net>
2012-06-19 23:34:39 -0400

    styles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  layout false
 
   def index
     @entries = Entry.all
--- db/migrate/20120619213537_create_entries.rb
+++ db/migrate/20120619213537_create_entries.rb
@@ -2,6 +2,7 @@ class CreateEntries < ActiveRecord::Migration
   def change
     create_table :entries do |t|
       t.string :computer_name
+      t.string :name
       t.boolean :testing, default: false
       t.datetime :last_had_the_ball_at
       t.timestamps
