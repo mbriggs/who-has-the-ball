c59ef321625863ea2382da2c02c26be4ede74ae6
Matt Briggs <matt@mattbriggs.net>
2012-06-20 01:32:09 -0400

    stub methods

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,11 @@
 class Entry < ActiveRecord::Base
   default_scope order('created_at ASC').limit(150)
+
+  def self.enqueue(name, computer)
+    raise NotImplementedError
+  end
+
+  def self.all_before(name)
+    raise NotImplementedError
+  end
 end
