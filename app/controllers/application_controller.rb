f12cae615a4f97363e00736caa7f997f143ab0e9
Matt Briggs <matt@mattbriggs.net>
2012-06-20 01:31:20 -0400

    adds some basic stuff around installation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   layout false
+  before_filter :set_host
 
   def index
     @entries = Entry.all
+  end
+
+  def install
+    render 'install.erb', type: 'text/plain'
+  end
+
+  def hydra
+    send_file Rails.root.join('cli', 'hydra'), type: 'text/plain'
+  end
+
+private
+
+  def set_host
+    @host = "http://localhost:3000"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 WhoHasTheBall::Application.routes.draw do
+  resource :entry
   root to: "application#index"
 end
